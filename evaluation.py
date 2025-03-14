3f345e5bd2d938d16b1c556289c27db6825622f6
lylebuist <102750153+lylebuist@users.noreply.github.com>
2025-03-14 18:07:38 +0000

    balancing

--- evaluation.py
+++ evaluation.py
@@ -9,6 +9,8 @@ avg_steps_taken = 0
 avg_decode_time = 0.0
 avg_backtrace_time = 0.0
 
+balance = 0
+
 files = files[1:]
 
 for file in files:
@@ -18,19 +20,27 @@ for file in files:
     steps_taken = int(file[5].split(' ')[-2][:-1])
     decode_time = float(file[6].split(' ')[-2][:-1])
     backtrace_time = float(file[7].split(' ')[-2][:-1])
+
+    len_expected = len(file[3].split(' ')) - 1
+    len_actual = len(file[4].split(' '))
     
     avg_wer += wer
     avg_steps_taken += steps_taken
     avg_decode_time += decode_time
     avg_backtrace_time += backtrace_time
 
+    balance += len_actual - len_expected
+
 avg_wer /= len(files)
 avg_steps_taken /= len(files)
 avg_decode_time /= len(files)
 avg_backtrace_time /= len(files)
+avg_balance = balance / len(files)
 
 print('Memory:', memory)
 print('Average WER:', avg_wer)
 print('Average Steps Taken:', avg_steps_taken)
 print('Average Decode Time:', avg_decode_time)
 print('Average Backtrace Time:', avg_backtrace_time)
+print('Balance:', balance)
+print('Average Balance:', avg_balance)
